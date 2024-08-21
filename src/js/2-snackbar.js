a74d5bd5e7af0ec087f5d94a23d3987a4b5746c4
Sergiy Humenniy <gum0372@gmail.com>
2024-08-21 23:12:51 +0300

    task-2 all

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,3 +1,39 @@
-import iziToast from "izitoast";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-import "izitoast/dist/css/iziToast.min.css";
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+    const state = form.elements.state.value;
+    
+     form.elements.delay.value = '';
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        position: 'topRight',
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        position: 'topRight',
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+});
